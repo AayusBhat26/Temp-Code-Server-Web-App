a4f48e6ea7c137780e1ffedf0b3eaecafdd1d812
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2022-07-02 14:54:26 +0530

    added the proces.env.port numner

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ ioS.on('connection', (socket)=>{
       
       
 })
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, ()=>{
       console.log(`Server running on port: ${PORT}`);
